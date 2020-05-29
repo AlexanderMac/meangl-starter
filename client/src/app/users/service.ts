30606473bc1f29fd26600ae3ed9dc8d279bfb47f
AlexanderMac <amatsibarov@gmail.com>
2020-05-29 11:48:43 +0300

    integrate graphql on the client side

--- client/src/app/users/service.ts
+++ client/src/app/users/service.ts
@@ -1,31 +1,114 @@
+import * as _ from 'lodash';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators';
 import { User } from './model';
-
-const USERS_URL = '/api/users/';
 
 @Injectable()
 export class UserService {
   constructor(private httpSrvc: HttpClient) {}
 
-  getUser(id: string): Observable<User> {
-    return this.httpSrvc.get<User>(USERS_URL + id);
+  getUser(userId: string): Observable<User> {
+    let query = `
+      query getUser($userId: ID!) {
+        user(userId: $userId) {
+          userId
+          name
+          email
+        }
+      }
+    `;
+    return this
+      ._postGLQuery(query, { userId })
+      .pipe(
+        map((data: any) => data.user)
+      );
   }
 
   getUsers(): Observable<User[]> {
-    return this.httpSrvc.get<User[]>(USERS_URL);
+    let query = `
+      query getUsers {
+        users {
+          userId
+          name
+          email
+        }
+      }
+    `;
+    return this
+      ._postGLQuery(query)
+      .pipe(
+        map((data: any) => data.users)
+      );
   }
 
-  createUser(user: User): Observable<User> {
-    return this.httpSrvc.post<User>(USERS_URL, user);
+  createUser(userData: User): Observable<User> {
+    let query = `
+      mutation createUser($userInput: UserInput!) {
+        createUser(input: $userInput) {
+          userId
+          name
+          email
+        }
+      }
+    `;
+    return this
+      ._postGLQuery(query, { userInput: userData })
+      .pipe(
+        map((data: any) => data.users)
+      );
   }
 
-  updateUser(userData: any): Observable<User> {
-    return this.httpSrvc.put<User>(USERS_URL + userData.userId, userData);
+  updateUser(userData: User): Observable<User> {
+    let query = `
+      mutation updateUser($userId: ID!, $userInput: UserInput!) {
+        updateUser(userId: $userId, input: $userInput) {
+          userId
+          name
+          email
+        }
+      }
+    `;
+    return this
+      ._postGLQuery(query, {
+        userId: userData.userId,
+        userInput: _.pick(userData, ['name', 'email'])
+      })
+      .pipe(
+        map((data: any) => data.users)
+      );
   }
 
-  deleteUser(id: string): Observable<boolean> {
-    return this.httpSrvc.delete<boolean>(USERS_URL + id);
+  deleteUser(userId: string): Observable<boolean> {
+    let query = `
+      mutation deleteUser($userId: ID!) {
+        deleteUser(userId: $userId)
+      }
+    `;
+    return this
+      ._postGLQuery(query, { userId })
+      .pipe(
+        map((data: any) => data.deleteUser)
+      );
+  }
+
+  private _postGLQuery(query: string, variables?: any): Observable<any> {
+    return this.httpSrvc
+      .post<User>(
+        '/graphql',
+        {
+          query,
+          variables
+        }
+      )
+      .pipe(
+        map((res: any) => {
+          if (res.errors) {
+            throw new Error(res.errors[0].message);
+          }
+          return res.data;
+        })
+      );
   }
 }
